0c0de520dc952b01933efbfe64a94b10cb8f09f1
K Srikanth <ksrikanth554@gmail.com>
2020-07-06 14:19:07 +0530

    printing

---
+++ lib/printing/printing_example.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class PrintingExmaple extends StatefulWidget {
+  @override
+  _PrintingExmapleState createState() => _PrintingExmapleState();
+}
+
+class _PrintingExmapleState extends State<PrintingExmaple> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Printing Example'),
+
+      ),
+      body: Center(
+        child: RaisedButton(
+
+        ),
+      ),
+    );
+  }
+}
